db361ff61d4f06abed60bb5d512daf907529fd2a
podgaysky <56561329+podgaysky@users.noreply.github.com>
2020-04-24 23:36:25 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,8 @@
-wget http://ftp.byfly.by/pub/archlinux/iso/2020.04.01/archlinux-2020.04.01-x86_64.iso.sig
-pacman-key -v archlinux-version-x86_64.iso.sig
+ls /sys/firmware/efi/efivars
+timedatectl set-ntp true
+fdisk -u -p /dev/whatever <<EOF
+n
+p
+1
+w
+EOF
