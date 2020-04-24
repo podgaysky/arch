a7d0d620c2f5700d19af6f32d9a403cde93db2e0
podgaysky <56561329+podgaysky@users.noreply.github.com>
2020-04-25 00:32:20 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,23 +1,24 @@
 ls /sys/firmware/efi/efivars
 timedatectl set-ntp true
 sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | fdisk /dev/sda
-g
-n
-1
-
-+550M
-t
-1
-n
-2
-
-+1024M
-t
-2
-19
-n
-3
-
-
-w
+g # create gtp partition table
+n # new partition
+1 # partition number 1
+  # default - start at beginning of disk
++550M # 550 MB efi parttion
+t # change parttion type
+1 # efi type
+n # new partition
+2 # partition number 2
+  # default - start at beginning of disk
++1024M # 1024 MB swap parttion
+t # change parttion type
+2 # partition number 2
+19 # swap type
+n # new partition
+3 # partition number 3
+  # default, start immediately after preceding partition
+  # default, extend partition to end of disk
+w # write the partition table
+q # and we're done
 EOF
