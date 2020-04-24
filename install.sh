417737a28c1b4d6c9678b23dec7209180feb1639
podgaysky <56561329+podgaysky@users.noreply.github.com>
2020-04-25 00:13:31 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,22 +1,22 @@
 ls /sys/firmware/efi/efivars
 timedatectl set-ntp true
 (
-echo g
-echo 1
-echo 
-echo +550M
-echo t
-echo 1
-echo n
-echo 2
-echo 
-echo +1024M
-echo t
-echo 2
-echo 19
-echo n
-echo 3
-echo 
-echo 
-echo w
+echo g #
+echo 1 #
+echo   #
+echo +550M #
+echo t #
+echo 1 #
+echo n #
+echo 2 #
+echo   #
+echo +1024M #
+echo t #
+echo 2 #
+echo 19 #
+echo n #
+echo 3 #
+echo   #
+echo   #
+echo w #
 ) | fdisk /dev/sda
