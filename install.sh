95917d96d33a428b2908b631d16868bb9c91df89
podgaysky <56561329+podgaysky@users.noreply.github.com>
2020-04-26 00:35:58 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,12 @@
 ls /sys/firmware/efi/efivars
-test=test
+echo -n Enter the name of the drive to install (for examples, sda):
+read drive_name
+echo -n Enter computer name:
+read computer_name
+echo -n Enter root password:
+read -s root_password
 timedatectl set-ntp true
-sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | fdisk /dev/sda
+sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | fdisk /dev/$drive_name
 g # create gtp partition table
 n # new partition
 1 # partition number 1
@@ -23,13 +28,13 @@ n # new partition
 w # write the partition table
 q # and we're done
 EOF
-mkfs.vfat /dev/sda1
-mkswap /dev/sda2
-swapon /dev/sda2
-mkfs.ext4 /dev/sda3
-mount /dev/sda3 /mnt
+mkfs.vfat /dev/$drive_name\1
+mkswap /dev/$drive_name\2
+swapon /dev/$drive_name\2
+mkfs.ext4 /dev/$drive_name\3
+mount /dev/$drive_name\3 /mnt
 mkdir /mnt/boot
-mount /dev/sda1 /mnt/boot
+mount /dev/$drive_name\1 /mnt/boot
 pacstrap /mnt base linux linux-firmware mc base-devel networkmanager intel-ucode
 genfstab -U /mnt >> /mnt/etc/fstab
 arch-chroot /mnt /bin/bash <<EOF
@@ -38,19 +43,17 @@ hwclock --systohc
 sed -i 's/# en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/g' ./locale.gen
 locale-gen
 echo "LANG=en_US.UTF-8" > /etc/locale.conf
-echo "arch" > /etc/hostname
+echo "$computer_name" > /etc/hostname
 echo "
 127.0.0.1	localhost
 ::1		localhost
-127.0.1.1	arch.localdomain	arch" >> /etc/hosts
+127.0.1.1	$computer_name.localdomain	$computer_name" >> /etc/hosts
 bootctl --path=/boot install
 echo "title Arch Linux
 linux /vmlinuz-linux
 initrd  /intel-ucode.img
 initrd /initramfs-linux.img" > /boot/loader/entries/arch.conf
-echo "options root=PARTUUID=$(blkid | grep /dev/sda3 | awk -F '"' '{print $2}') rw" >> /boot/loader/entries/arch.conf
-echo "root:temppass" | chpasswd
-echo $test
+echo "options root=UUID=$(blkid | grep /dev/$drive_name\3 | awk -F '"' '{print $2}') rw" >> /boot/loader/entries/arch.conf
+echo "root:$root_password" | chpasswd
+systemctl enable NetworkManager
 EOF
-
-
