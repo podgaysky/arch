94ee9a6561c34f0d7824744a05f8c293f6859a0c
podgaysky <56561329+podgaysky@users.noreply.github.com>
2020-04-26 00:53:05 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 ls /sys/firmware/efi/efivars
-echo -n Enter the name of the drive to install (for examples, sda):
+echo -n "Enter the name of the drive to install (for examples, sda):"
 read drive_name
-echo -n Enter computer name:
+echo -n "Enter computer name:"
 read computer_name
-echo -n Enter root password:
+echo -n "Enter root password:"
 read -s root_password
-echo -n Confirm root password:
+echo -n "Confirm root password:"
 read -s root_password_confirm
 if [ $root_password -ne $root_password_confirm ] ; then echo "passwords do not match run the script again" && exit 1 ; fi
 timedatectl set-ntp true
