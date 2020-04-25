2776ecdd4f1e88720c1eb3814a93ebfd005674e3
podgaysky <56561329+podgaysky@users.noreply.github.com>
2020-04-25 07:35:17 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,31 +5,40 @@ g # create gtp partition table
 n # new partition
 1 # partition number 1
   # default - start at beginning of disk
-+550M # 550 MB boot parttion
++550M # 550 MB boot partition
 t # change parttion type
 4 # boot type
 n # new partition
 2 # partition number 2
   # default - start at beginning of disk
-+1024M # 1024 MB swap parttion
++550M # 550 MB efi partition
 t # change parttion type
 2 # partition number 2
-19 # swap type
+1 # efi type
 n # new partition
 3 # partition number 3
+  # default - start at beginning of disk
++1024M # 1024 MB swap parttion
+t # change parttion type
+3 # partition number 3
+19 # swap type
+n # new partition
+4 # partition number 4
   # default, start immediately after preceding partition
   # default, extend partition to end of disk
 w # write the partition table
 q # and we're done
 EOF
-mkfs.fat -F32 /dev/sda1
-mkswap /dev/sda2
-swapon /dev/sda2
-mkfs.ext4 /dev/sda3
-mount /dev/sda3 /mnt
+mkfs.ext2 /dev/sda1
+mkfs.fat -F32 /dev/sda2
+mkswap /dev/sda3
+swapon /dev/sda3
+mkfs.ext4 /dev/sda4
+mount /dev/sda4 /mnt
 mkdir /mnt/boot
 mount /dev/sda1 /mnt/boot
 mkdir /mnt/boot/efi
+mount /dev/sda3 /mnt/boot/efi
 pacstrap /mnt base linux linux-firmware mc base-devel networkmanager os-prober dialog wpa_supplicant efibootmgr
 genfstab -U /mnt >> /mnt/etc/fstab
 arch-chroot /mnt
